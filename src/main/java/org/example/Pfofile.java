51c8b374b5c314aa32634e1b3f74f1d333bfafc2
BohdanBokii <bohdanbwork95@gmail.com>
2023-05-09 13:10:59 +0300

    --first commit

---
+++ src/main/java/org/example/Pfofile.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Pfofile {
+    public static void main(String[] args) {
+        System.out.println("Бокій Богдан Ігорович"+ "\n" +"01.11.1995"+ "\n" +"Java developer");
+    }
+}
